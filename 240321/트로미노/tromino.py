3efb9a353f424b473d7f41a1691a2b46642afdb2
LetzGoRats <77396189+letzgorats@users.noreply.github.com>
2024-03-21 01:09:37 +0900

    Title: 트로미노, Time: 143ms, Memory: 25MB, Status: Wrong Answer - CodeTree

---
+++ 240321/트로미노/tromino.py
@@ -0,0 +1,49 @@
+n,m = map(int,input().split())
+
+board = [list(map(int,input().split())) for _ in range(n)]
+
+
+def case1(i,j):
+
+    return board[i][j] + board[i-1][j] + board[i][j+1]
+
+def case2(i,j):
+
+    return board[i][j] + board[i+1][j] + board[i][j+1]
+
+def case3(i,j):
+
+    return board[i][j] + board[i+1][j] + board[i][j-1]
+
+
+def case4(i,j):
+
+    return board[i][j+1] + board[i][j] + board[i][j+2]
+
+def case5(i,j):
+
+    return board[i][j] + board[i+1][j] + board[i+2][j]
+
+
+answer = 0
+
+for i in range(n):
+    for j in range(m):
+
+        if 0<= i-1 < n and 0<= j+1 < m:
+            answer = max(answer,case1(i,j))
+
+        if 0<= i+1 < n and 0<= j+1 < m:
+            answer = max(answer,case2(i,j))
+
+        if 0<= i+1 < n and 0<= j-1 < m:
+            answer = max(answer,case3(i,j))
+        
+        if 0<= j+1 < m and 0<= j+2 < m:
+            answer = max(answer,case4(i,j))
+
+        if 0<= i+1 < n and 0<=i+2 < n:
+            answer = max(answer,case5(i,j)) 
+
+
+print(answer)
