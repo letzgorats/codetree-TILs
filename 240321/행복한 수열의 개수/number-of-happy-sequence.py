224c046d5e10cf1cd85e1c261e0ce2eb605a5e58
LetzGoRats <77396189+letzgorats@users.noreply.github.com>
2024-03-21 00:13:21 +0900

    Title: 행복한 수열의 개수, Time: 117ms, Memory: 25MB, Status: Wrong Answer - CodeTree

--- 240321/행복한 수열의 개수/number-of-happy-sequence.py
+++ 240321/행복한 수열의 개수/number-of-happy-sequence.py
@@ -3,23 +3,26 @@ n, m = map(int,input().split())
 board = [list(map(int,input().split())) for _ in range(n)]
 # print(board)
 
-answer = 0
-for i in range(n):
-    cnt = 1
-    for j in range(n-1):
-        if board[i][j] == board[i][j+1]:
-            cnt += 1
-        if cnt == m:
-            answer += 1
-            break
-# print(answer)
-for j in range(n):
-    cnt = 1
-    for i in range(n-1):
-        if board[i][j] == board[i+1][j]:
-            cnt += 1
-        if cnt == m:
-            answer += 1
-            break
+if m == 1:
+    print(2*n)
+else:
+    answer = 0
+    for i in range(n):
+        cnt = 1
+        for j in range(n-1):
+            if board[i][j] == board[i][j+1]:
+                cnt += 1
+            if cnt == m:
+                answer += 1
+                break
+    # print(answer)
+    for j in range(n):
+        cnt = 1
+        for i in range(n-1):
+            if board[i][j] == board[i+1][j]:
+                cnt += 1
+            if cnt == m:
+                answer += 1
+                break
 
-print(answer)
+    print(answer)
