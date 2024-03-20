93a00024f59daeae6c73dc174a2a9432037afa1d
LetzGoRats <77396189+letzgorats@users.noreply.github.com>
2024-03-21 01:22:19 +0900

    Title: 트로미노, Time: 171ms, Memory: 25MB, Status: Passed - CodeTree

--- 240321/트로미노/tromino.py
+++ 240321/트로미노/tromino.py
@@ -2,54 +2,28 @@ n,m = map(int,input().split())
 
 board = [list(map(int,input().split())) for _ in range(n)]
 
-
-def case1(i,j):
-
-    return board[i][j] + board[i+1][j] + board[i][j+1]
-
-def case2(i,j):
-
-    return board[i][j-1] + board[i][j] + board[i+1][j]
-
-def case3(i,j):
-
-    return board[i-1][j] + board[i][j] + board[i][j-1]
-
-def case4(i,j):
-
-    return board[i-1][j] + board[i][j] + board[i][j+1]
-
-def case5(i,j):
-
-    return board[i][j+1] + board[i][j] + board[i][j+2]
-
-def case6(i,j):
-
-    return board[i][j] + board[i+1][j] + board[i+2][j]
-
-
 answer = 0
 
 for i in range(n):
     for j in range(m):
 
         if 0<= i+1 < n and 0<= j+1 < m:
-            answer = max(answer,case1(i,j))
+            answer = max(answer,board[i][j] + board[i+1][j] + board[i][j+1])
 
-        if 0<= i+1 < n and 0<= j-1 < m:
-            answer = max(answer,case2(i,j))
+        if 0<= i+1 < n and 0<= j-1 < m and 0<= j+1 < m:
+            answer = max(answer,board[i][j-1] + board[i][j] + board[i+1][j])
 
         if 0<= i-1 < n and 0<= j-1 < m:
-            answer = max(answer,case3(i,j))
+            answer = max(answer,board[i-1][j] + board[i][j] + board[i][j-1])
         
         if 0<= i-1 < n and 0<= j+1 < m:
-            answer = max(answer,case3(i,j))
+            answer = max(answer,board[i-1][j] + board[i][j] + board[i][j+1])
         
         if 0<= j+1 < m and 0<= j+2 < m:
-            answer = max(answer,case5(i,j))
+            answer = max(answer,board[i][j+1] + board[i][j] + board[i][j+2])
 
         if 0<= i+1 < n and 0<=i+2 < n:
-            answer = max(answer,case6(i,j)) 
+            answer = max(answer,board[i][j] + board[i+1][j] + board[i+2][j]) 
 
 
 print(answer)
