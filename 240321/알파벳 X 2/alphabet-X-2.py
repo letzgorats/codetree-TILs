f68f5f66176f8b95676d9a56da230156d4e4888d
LetzGoRats <77396189+letzgorats@users.noreply.github.com>
2024-03-21 23:56:28 +0900

    Title: 알파벳 X 2, Time: 140ms, Memory: 24MB, Status: Passed - CodeTree

--- 240321/알파벳 X 2/alphabet-X-2.py
+++ 240321/알파벳 X 2/alphabet-X-2.py
@@ -16,7 +16,7 @@ count = 0
 for i in range(len(first_appear)-1):
     for j in range(i+1,len(first_appear)):
         a_start, a_end = index[first_appear[i]]
-        b_start, b_end = index[first_appear[i]]
+        b_start, b_end = index[first_appear[j]]
         if a_start < b_start < a_end < b_end or b_start < a_start < b_end < a_end:
             count += 1
 
