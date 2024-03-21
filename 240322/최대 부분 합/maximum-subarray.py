9c97f47468c432d4746d8057cad71e108a3034bb
LetzGoRats <77396189+letzgorats@users.noreply.github.com>
2024-03-22 01:15:11 +0900

    Title: 최대 부분 합, Time: 108ms, Memory: 24MB, Status: Passed - CodeTree

--- 240322/최대 부분 합/maximum-subarray.py
+++ 240322/최대 부분 합/maximum-subarray.py
@@ -1,9 +1,19 @@
+# 7
+# 1 -2 4 5 -6 7 -2   
+
 n = int(input())
 numbers = list(map(int,input().split()))
 
-prefix_sum = [0] * (n+1)
+# Kadane 알고리즘
+max_so_far = numbers[0]
+max_ending_here = numbers[0]
 
-for i in range(n):
-    prefix_sum[i+1] = prefix_sum[i] + numbers[i]
+for x in numbers[1:]:
+    max_ending_here = max(x,max_ending_here+x)
+    # print(max_ending_here)
+    max_so_far = max(max_so_far,max_ending_here)
+    # print(max_so_far)
 
-print(prefix_sum[-1])
+print(max_so_far)
+
+# prefix suffix 알고리즘
