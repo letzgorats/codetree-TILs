75ea2f719677f19ea5d11d90db895a697b9161e2
LetzGoRats <77396189+letzgorats@users.noreply.github.com>
2024-03-21 23:54:21 +0900

    Title: 알파벳 X 2, Time: 105ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240321/알파벳 X 2/alphabet-X-2.py
+++ 240321/알파벳 X 2/alphabet-X-2.py
@@ -6,12 +6,24 @@ first_appear = []
 
 for i, a in enumerate(alphabets):
 
-    if a not in first_appear:
+    if a not in index:
         first_appear.append(a)
-        index[a] = i
+        index[a] = [i]
     else:
-        tmp = i - index[a]
-        index[a] = tmp
+        index[a].append(i)
 
-# print(index)
-print(len(list(set(index.values())))-1)
+count = 0
+for i in range(len(first_appear)-1):
+    for j in range(i+1,len(first_appear)):
+        a_start, a_end = index[first_appear[i]]
+        b_start, b_end = index[first_appear[i]]
+        if a_start < b_start < a_end < b_end or b_start < a_start < b_end < a_end:
+            count += 1
+
+print(count)
+
+
+# for i in list(set(index.values())):
+#     if i != 1:
+
+# print(len()-1)
