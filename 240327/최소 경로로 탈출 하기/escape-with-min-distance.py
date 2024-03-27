ae48a7ca7df3d2bdfe4c16731af4c565d0ef26f0
LetzGoRats <77396189+letzgorats@users.noreply.github.com>
2024-03-27 20:50:39 +0900

    Title: 최소 경로로 탈출 하기, Time: 239ms, Memory: 25MB, Status: Passed - CodeTree

--- 240327/최소 경로로 탈출 하기/escape-with-min-distance.py
+++ 240327/최소 경로로 탈출 하기/escape-with-min-distance.py
@@ -1,34 +1,36 @@
 from collections import deque
+import copy
 
 n, m = map(int,input().split())
 board = []
 for _ in range(n):
     board.append(list(map(int,input().split())))
 
-def bfs(r,c,count):
+def bfs(r,c):
 
-    deq = deque([(r,c,count)])
-    # visited = [[False] * m for _ in range(n)]
-    board[r][c] = 0 
-    # visited[r][c] = True
+    if board[r][c] == 0:
+        return -1
+    
+    deq = deque([(r,c,0)])
+    visited = [[False] * m for _ in range(n)]
+    visited[r][c] = True
     dr = [-1,1,0,0]
     dc = [0,0,-1,1]
+    
     while deq:
 
-        r, c, cnt = deq.popleft()
+        r, c ,cnt = deq.popleft()
+
+        if r == n-1 and c == m-1:
+            return cnt
 
         for d in range(4):
             nr, nc = r + dr[d], c + dc[d]
-            if 0<= nr < n and 0<= nc < m and board[nr][nc] == 1 :
-            # and not visited[nr][nc]:
-                # visited[nr][nc] = True
-                deq.append((nr,nc,cnt+1))
-                board[nr][nc] = 0
+            if 0<= nr < n and 0<= nc < m and board[nr][nc] == 1 and not visited[nr][nc]:
+                visited[nr][nc] = True
+                deq.append((nr,nc,cnt+1))      
 
-    if r == n-1 and c == m-1 :
-        return cnt
-    else:
-        return -1
+    return -1
 
 
-print(bfs(0,0,0))
+print(bfs(0,0))
