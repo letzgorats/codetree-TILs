7644f998c4f0d66dddf05bc86b808d8dded41a52
LetzGoRats <77396189+letzgorats@users.noreply.github.com>
2024-04-02 17:11:31 +0900

    Title: 컨베이어 벨트, Time: 119ms, Memory: 25MB, Status: Passed - CodeTree

--- 240402/컨베이어 벨트/conveyor-belt.py
+++ 240402/컨베이어 벨트/conveyor-belt.py
@@ -2,14 +2,12 @@ n, t = map(int,input().split())
 belt = list(map(int,input().split()))
 belt.extend(list(map(int,input().split())))
 
-# print(belt)
-
 temp = belt[-1]
 for _ in range(t):
     for i in range(2*n-1,0,-1):
         belt[i] = belt[i-1]
-
-belt[0] = temp
+    belt[0] = temp
+    temp = belt[-1]
 
 print(*belt[:n])
 print(*belt[n:])
