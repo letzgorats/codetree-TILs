8757708773d33588f044241beeae54e4696b3faf
LetzGoRats <77396189+letzgorats@users.noreply.github.com>
2024-04-08 13:28:57 +0900

    Title: 겹치지 않게 선분 고르기, Time: 185ms, Memory: 33MB, Status: Passed - CodeTree

--- 240408/겹치지 않게 선분 고르기/select-segments-without-overlap.py
+++ 240408/겹치지 않게 선분 고르기/select-segments-without-overlap.py
@@ -9,22 +9,20 @@ for _ in range(n):
 
 lines = sorted(lines,key= lambda x : x[1])
 # print(lines)
-max_count = 0
+max_count = [0]
 
-def backtracking(lines, index, last_end,count):
+def backtracking(lines, index, last_end,count,max_count):
 
     if len(lines) == index:
-        return count
+        max_count[0] = max(count, max_count[0])
+        return 
         
     # 선분 선택하지 않는 경우
-    not_chosen = backtracking(lines,index+1,last_end,count)
+    backtracking(lines,index+1,last_end,count,max_count)
     # 선분을 선택하는 경우
     if lines[index][0] > last_end:
-        chosen = backtracking(lines,index+1,lines[index][1],count+1)
-    else:
-        chosen = count
+        backtracking(lines,index+1,lines[index][1],count+1,max_count)
 
-    return max(chosen,not_chosen)
 
-max_count = backtracking(lines,0,-1,0)
-print(max_count)
+backtracking(lines,0,-1,0,max_count)
+print(*max_count)
