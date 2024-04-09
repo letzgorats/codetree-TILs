4acb5d0fca749aeb3c9a20133b312d7cd4e1c1da
LetzGoRats <77396189+letzgorats@users.noreply.github.com>
2024-04-10 07:38:38 +0900

    Title: 특정 조건에 맞게 k개 중에 1개를 n번 뽑기, Time: 100ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240410/특정 조건에 맞게 k개 중에 1개를 n번 뽑기/n-permutations-of-k-with-repetition-under-constraint.py
@@ -0,0 +1,18 @@
+k, n = map(int,input().split())
+
+def backtracking(path, last, count, n, k):
+
+    if len(path) == n:
+        print(''.join(map(str,path)))
+        return 
+    
+    for next_num in range(1,k+1):
+        if next_num == last and count == 2: # 연속 3회 조건 체크
+            continue
+        if next_num == last:
+            backtracking(path + [next_num], next_num, count+1, n, k)
+        else:
+            backtracking(path + [next_num], next_num, 1, n, k)
+
+
+backtracking([],0,0,n,k)
