4dea997889466a8646d7a8f63e625c59914988fb
LetzGoRats <77396189+letzgorats@users.noreply.github.com>
2024-04-10 06:40:50 +0900

    Title: 안전 지대, Time: 185ms, Memory: 27MB, Status: Passed - CodeTree

--- 240410/안전 지대/comfort-zone.py
+++ 240410/안전 지대/comfort-zone.py
@@ -7,25 +7,22 @@ for i in range(n):
 
 def dfs(r,c,k):
 
-    if not (0<= r < n and 0<= c < m) or visited[r][c] or board[r][c] <= k:
-        return 
-
-    visited[r][c] = True
-
-    for d in range(4):
-        nr = r + dr[d]
-        nc = c + dc[d]
-        dfs(nr,nc,k)
+    stack = [(r, c)]
+    while stack:
+        r, c = stack.pop()
+        if r < 0 or r >= n or c < 0 or c >= m or visited[r][c] or board[r][c] <= k:
+            continue
+        visited[r][c] = True
+        stack.extend([(r-1, c), (r+1, c), (r, c-1), (r, c+1)])
 
 
-area = 0
 max_area = 0
 min_k = 100
 
 for k in range(1, max(map(max, board)) + 1):
 
-    dr = [-1,1,0,0]
-    dc = [0,0,-1,1]
+    # dr = [-1,1,0,0]
+    # dc = [0,0,-1,1]
     
     visited = [[False] * m for _ in range(n)]
     area = 0
